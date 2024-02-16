0e4d303dbf99b58c45d2c90b1433f0e9beed627d
Bringtoas <48325790+AsmitaSharma016@users.noreply.github.com>
2024-02-15 22:36:00 -0500

    Add files via upload

---
+++ Assignment2.cs
@@ -0,0 +1,34 @@
+﻿//Author: Asmita Sharma 899010
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x;
+
+        while (true)
+        {
+            Console.Write("Enter a real number (or type 'exit' to terminate): "); // Prompts a user to enter a real number
+
+            string input = Console.ReadLine();
+            if (input.ToLower() == "exit") // Checks if the user wants to exit
+                break;
+
+            if (!double.TryParse(input, out x)) // Checks input is valid real number
+            {
+                Console.WriteLine("Invalid input. Please enter a valid real number.");
+                continue;
+            }
+
+            if ((x >= 2 && x < 3) || (x >= 0 && x < 1) || (x >= -10 && x <= -2))
+            {
+                Console.WriteLine("x belongs to I");
+            }
+            else
+            {
+                Console.WriteLine("x does not belong to I");
+            }
+        }
+    }
+}
